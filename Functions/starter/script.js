188fbec4adf3784b708794c75db5a1988f5f2397
Ahsanul Mostakin <ahsanulmostakin1111.com>
2024-02-27 22:05:39 +0600

    Function

--- Functions/starter/script.js
+++ Functions/starter/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+const bookings = [];
+const creatBooking = function (
+  flightNum,
+  numberPassengers = 1,
+  price = 199 * numberPassengers
+) {
+  const booking = {
+    flightNum,
+    numberPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+creatBooking('LH1');
+creatBooking('LH123', 2, 1000);
+creatBooking('LH123', 5);
