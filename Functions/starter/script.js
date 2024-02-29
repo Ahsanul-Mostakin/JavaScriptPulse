091e7a7280d170e0cfc4efefc16e91ad9e6d9c48
Ahsanul Mostakin <ahsanulmostakin1111.com>
2024-02-29 18:53:41 +0600

    function

--- Functions/starter/script.js
+++ Functions/starter/script.js
@@ -18,3 +18,24 @@ const creatBooking = function (
 creatBooking('LH1');
 creatBooking('LH123', 2, 1000);
 creatBooking('LH123', 5);
+
+const flight = 'LH234';
+const mostakin = {
+  name: 'Ahsanul Mostakin',
+  passport: 123456789,
+};
+
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr .' + passenger.name;
+
+  if (passenger.passport === 123456789) {
+    alert('Checked in');
+  } else {
+    alert('Wrong passport');
+  }
+};
+
+checkIn(flight, mostakin);
+console.log(flight);
+console.log(mostakin);
