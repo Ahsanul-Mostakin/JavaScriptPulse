da244b7b801da29458cde7a39b7d3ee733b8072e
Ahsanul Mostakin <ahsanulmostakin1111.com>
2024-03-01 23:56:20 +0600

    function

--- Functions/starter/script.js
+++ Functions/starter/script.js
@@ -1,41 +1,45 @@
 'use strict';
 
-const bookings = [];
-const creatBooking = function (
-  flightNum,
-  numberPassengers = 1,
-  price = 199 * numberPassengers
-) {
-  const booking = {
-    flightNum,
-    numberPassengers,
-    price,
-  };
-  console.log(booking);
-  bookings.push(booking);
+// const bookings = [];
+// const creatBooking = function (
+//   flightNum,
+//   numberPassengers = 1,
+//   price = 199 * numberPassengers
+// ) {
+//   const booking = {
+//     flightNum,
+//     numberPassengers,
+//     price,
+//   };
+//   console.log(booking);
+//   bookings.push(booking);
+// };
+
+// creatBooking('LH1');
+// creatBooking('LH123', 2, 1000);
+// creatBooking('LH123', 5);
+
+// const flight = 'LH234';
+// const mostakin = {
+//   name: 'Ahsanul Mostakin',
+//   passport: 123456789,
+// };
+
+// const checkIn = function (flightNum, passenger) {
+//   flightNum = 'LH999';
+//   passenger.name = 'Mr .' + passenger.name;
+
+//   if (passenger.passport === 123456789) {
+//     alert('Checked in');
+//   } else {
+//     alert('Wrong passport');
+//   }
+// };
+
+// checkIn(flight, mostakin);
+// console.log(flight);
+// console.log(mostakin);
+
+const oneWord = function (str) {
+  return str.replace(/ / getComputedStyle,  ' ' .toLowerCase() );
 };
-
-creatBooking('LH1');
-creatBooking('LH123', 2, 1000);
-creatBooking('LH123', 5);
-
-const flight = 'LH234';
-const mostakin = {
-  name: 'Ahsanul Mostakin',
-  passport: 123456789,
-};
-
-const checkIn = function (flightNum, passenger) {
-  flightNum = 'LH999';
-  passenger.name = 'Mr .' + passenger.name;
-
-  if (passenger.passport === 123456789) {
-    alert('Checked in');
-  } else {
-    alert('Wrong passport');
-  }
-};
-
-checkIn(flight, mostakin);
-console.log(flight);
-console.log(mostakin);
