3b70ebe56879d6eeed903e3497121daa0fcdd22e
Ahsanul Mostakin <ahsanulmostakin1111.com>
2024-04-05 04:41:11 +0600

    Data structure

--- Data-Structures-Operators/starter/script.js
+++ Data-Structures-Operators/starter/script.js
@@ -12,6 +12,10 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
+  order: function(starterIndex,mainIndex){
+    return [this.starterMenu[starterIndex],this.mainMenu[mainIndex]]; 
+  },
+
   openingHours: {
     thu: {
       open: 12,
@@ -35,4 +39,26 @@ const c = arr[2];
 const [x,y,z] = arr;
 console.log(x,y,z);
 console.log(arr);
+ 
+let[main, , secondary] = restaurant.categories;
+console.log(main,secondary);
+//switching variable
+// const temp = main;
+// main = secondary;
+// secondary = temp;
+// console.log(main, secondary);
+
+[main, secondary] = [secondary, main];
+console.log(main,secondary);
+
+const[starter,mainCourse] = restaurant.order(2,0);
+console.log(starter,mainCourse);
+
+const nested = [2,4,[5,6]];
+// const [i,j]=nested;
+// console.log(i,j);
+const [i, , [j,k]] =nested;
+console.log(i,j,k);
+
+
 
