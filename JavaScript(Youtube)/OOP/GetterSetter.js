d70e421322f2fc0a0cb5b1519e8c2b7e71346d68
Ahsanul Mostakin <ahsanulmostakin1111@gmail.com>
2025-01-30 21:16:35 +0600

    Getter & Setter

---
+++ JavaScript(Youtube)/OOP/GetterSetter.js
@@ -0,0 +1,18 @@
+class Product{
+
+    set SetPrice(value){
+        this.price = value;
+    }
+
+    get GetPrice (){
+        return this.price;
+    }
+
+}
+
+let ProductObj = new Product();
+
+ProductObj.SetPrice=100;
+
+console.log(ProductObj.GetPrice);
+
